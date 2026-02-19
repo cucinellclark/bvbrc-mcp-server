08f9be906dcdebe61a531377c105322ff2fb725c
cucinellclark <cucinell@anl.gov>
2026-02-19 14:51:08 -0600

    remove tracking rag_api

--- rag_api/app/routers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .query import router as query_router
-from .databases import router as databases_router
-from .health import router as health_router
-
--- rag_api/app/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .embedding_service import EmbeddingService
-from .rag_service import RAGService
-from .database_manager import DatabaseManager
-
